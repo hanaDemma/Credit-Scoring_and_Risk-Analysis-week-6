c9ab509c0ac818c162e77cd0656e38271545c42d
hana demma <hana.demma@gmail.com>
2025-01-24 18:07:13 +0300

    one hot encoding

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder
+
 def aggregateFeatures(data):
     """
        Aggregates transaction data by customer.
@@ -37,3 +42,21 @@ def extractDateAndTime(new_dataframe):
     new_dataframe['TransactionMonth'] = new_dataframe['TransactionStartTime'].dt.month
     new_dataframe['TransactionYear'] = new_dataframe['TransactionStartTime'].dt.year
     return new_dataframe
+
+def encodingCategoricalVariables(new_dataframe):
+    """
+        Encodes categorical variables using OneHotEncoding.
+
+        Parameters:
+            new_dataframe (pd.DataFrame): Dataset containing categorical columns.
+
+        Returns:
+            pd.DataFrame: Dataset with encoded categorical variables.
+        """
+    categorical_columns = ['CurrencyCode', 'ProductCategory']
+    encoder = OneHotEncoder(sparse_output=False, drop='first')
+    encoded_data = encoder.fit_transform(new_dataframe[categorical_columns])
+    encoded_new_dataframe = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out(categorical_columns))
+    new_dataframe_encoded = pd.concat([new_dataframe.reset_index(drop=True), encoded_new_dataframe], axis=1)
+    new_dataframe_encoded.drop(columns=categorical_columns, inplace=True)
+    return new_dataframe_encoded
