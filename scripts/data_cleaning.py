4626b21e95d2f09d112cf4eaf0940925123f471e
hana demma <hana.demma@gmail.com>
2025-01-24 17:16:54 +0300

    missing value

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def display_skewness(data):
     skew_kurt_summary = {}
@@ -10,3 +11,26 @@ def display_skewness(data):
         }
         print(f"{col}\nSkewness: {data[col].skew()}\nKurtosis: {data[col].kurt()}")
     return skew_kurt_summary
+
+def find_missing_values(df):
+    """
+        Finds and summarizes missing values in the dataset.
+
+        Parameters:
+            df (pd.DataFrame): The dataset to analyze.
+
+        Returns:
+            pd.DataFrame: Summary table of missing values, percentages, and data types.
+        """
+    null_counts = df.isnull().sum()
+    missing_value = null_counts
+    percent_of_missing_value = 100 * null_counts / len(df)
+    data_type = df.dtypes
+
+    missing_data_summary = pd.concat([missing_value, percent_of_missing_value, data_type], axis=1)
+    missing_data_summary_table = missing_data_summary.rename(columns={0: "Missing values", 1: "Percent of Total Values", 2: "DataType"})
+    missing_data_summary_table = missing_data_summary_table[missing_data_summary_table.iloc[:, 1] != 0].sort_values('Percent of Total Values', ascending=False).round(1)
+
+    print(f"From {df.shape[1]} columns selected, there are {missing_data_summary_table.shape[0]} columns with missing values.")
+
+    return missing_data_summary_table
