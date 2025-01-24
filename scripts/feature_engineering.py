e66c765ca4bfa9cf7f48dae8c124c58e75d336d6
hana demma <hana.demma@gmail.com>
2025-01-24 17:49:57 +0300

    extract date and time

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def aggregateFeatures(data):
     """
        Aggregates transaction data by customer.
@@ -16,3 +17,23 @@ def aggregateFeatures(data):
         StdTransactionAmount=('Amount', 'std')
     ).reset_index()
     return agg_data
+
+def extractDateAndTime(new_dataframe):
+    """
+        Extracts date and time features from a datetime column.
+
+        Parameters:
+            new_dataframe (pd.DataFrame): Dataset containing a datetime column 'TransactionStartTime'.
+
+        Returns:
+            pd.DataFrame: Dataset with new time-based features.
+        """
+    # Converting TransactionStartTime to datetime format
+    new_dataframe['TransactionStartTime'] = pd.to_datetime(new_dataframe['TransactionStartTime'])
+
+    # Extracting date and time-based features
+    new_dataframe['TransactionHour'] = new_dataframe['TransactionStartTime'].dt.hour
+    new_dataframe['TransactionDay'] = new_dataframe['TransactionStartTime'].dt.day
+    new_dataframe['TransactionMonth'] = new_dataframe['TransactionStartTime'].dt.month
+    new_dataframe['TransactionYear'] = new_dataframe['TransactionStartTime'].dt.year
+    return new_dataframe
